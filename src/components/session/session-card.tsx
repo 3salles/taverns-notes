69e3d587955cd4948fad1dd1dab0c4b2e3eb2f9a
Beatriz Salles <beatrizsallesss@gmail.com>
2026-03-04 18:44:22 -0300

    feat: ✨ Create delete session button

--- src/components/session/session-card.tsx
+++ src/components/session/session-card.tsx
@@ -1,30 +1,85 @@
+'use client';
 import { ISessionSummary } from '@/core/domain/sessions/session.entity';
+import { Trash as DeleteIcon, Loader2 as LoadingIcon } from 'lucide-react';
 import Link from 'next/link';
+import { useState } from 'react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '../ui/alert-dialog';
+import { Button } from '../ui/button';
 
 export interface SessionCardProps {
   session: ISessionSummary;
 }
 
 export const SessionCard = ({ session }: SessionCardProps) => {
-  return (
-    <li
-      className="p-3 rounded-lg transition-all duration-200 group relative
-     hover:bg-gray-700"
-    >
-      <header className="flex items-start justify-between">
-        <Link href={`/${session.id}`} prefetch className="flex-1 min-w-0">
-          <h3
-            className="font-medium text-sm text-white 
-        group-hover:text-accent-300 transition-colors"
-          >
-            {session.title}
-          </h3>
+  const [isDeleting, setIsDeleting] = useState(false);
 
-          <p className="text-xs text-gray-400 mt-1 line-clamp-2">
-            {session.note}
-          </p>
-        </Link>
-      </header>
-    </li>
+  const handleDeleteSession = () => setIsDeleting(true);
+
+  return (
+    <>
+      <li
+        className="p-3 rounded-lg transition-all duration-200 group relative
+     hover:bg-gray-700"
+      >
+        <header className="flex items-start justify-between">
+          <Link href={`/${session.id}`} prefetch className="flex-1 min-w-0">
+            <h3
+              className="font-medium text-sm text-white 
+        group-hover:text-accent-300 transition-colors"
+            >
+              {session.title}
+            </h3>
+
+            <p className="text-xs text-gray-400 mt-1 line-clamp-2">
+              {session.note}
+            </p>
+          </Link>
+
+          <AlertDialog>
+            <AlertDialogTrigger asChild>
+              <Button
+                variant="icon"
+                size="icon"
+                title="Remover sessão"
+                aria-label="Remover sessão"
+                onClick={() => setIsDeleting(true)}
+              >
+                <DeleteIcon className="w-3 h-3" />
+              </Button>
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+              <AlertDialogHeader>
+                <AlertDialogTitle>Remover Sessão</AlertDialogTitle>
+                <AlertDialogDescription>
+                  Tem certeza que deseja remover esta sessão? Esta ação não pode
+                  ser desfeita.
+                </AlertDialogDescription>
+              </AlertDialogHeader>
+              <AlertDialogFooter>
+                <AlertDialogCancel>Cancelar</AlertDialogCancel>
+                <AlertDialogAction
+                  onClick={handleDeleteSession}
+                  disabled={isDeleting}
+                >
+                  {isDeleting && (
+                    <LoadingIcon className="mr-2 h-4 w-4 animate-spin" />
+                  )}
+                </AlertDialogAction>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialog>
+        </header>
+      </li>
+    </>
   );
 };
--- src/tests/infra/components/session/session-card.spec.tsx
+++ src/tests/infra/components/session/session-card.spec.tsx
@@ -3,12 +3,14 @@ import {
   SessionCardProps,
 } from '@/components/session/session-card';
 import { render, screen } from '@/lib/test-utils';
+import userEvent from '@testing-library/user-event';
 
 const renderSut = ({ session }: SessionCardProps) => {
   return render(<SessionCard session={session} />);
 };
 
 describe('SessionCard', () => {
+  const user = userEvent.setup();
   const session = { id: '1', title: 'title 01', note: 'content 01' };
 
   it('deveria renderizar o link com href corretamente', () => {
@@ -17,5 +19,15 @@ describe('SessionCard', () => {
 
     expect(link).toBeInTheDocument();
     expect(link).toHaveAttribute('href', `/${session.id}`);
+  });
+
+  it('should open dialog to delete session', async () => {
+    renderSut({ session });
+
+    const deleteButton = screen.getByRole('button', { name: 'Remover sessão' });
+
+    await user.click(deleteButton);
+
+    expect(screen.getByText('Remover Sessão')).toBeInTheDocument();
   });
 });
